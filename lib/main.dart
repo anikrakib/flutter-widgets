21628c9b3eb4f358dd3a9f217a20dbb429805ff4
Anik Rakib <anikrakib1998@gmail.com>
2021-12-29 17:48:18 +0600

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    title: "Awesome App",
+    home: HomePage(),
+    debugShowCheckedModeBanner: false,
+  ));
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Awesome App"),
+      ),
+      body: Container(
+        child: Center(child: Text("Hi Flutter")),
+      ),
+    );
+  }
+}
