0e2c2e6ab59706acd7e1e9ae8fc82cc84943900b
Anik Rakib <anikrakib1998@gmail.com>
2022-01-06 04:25:16 +0600

    custom animated fav menu

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_widget/appBar/appbar_with_tab_layout.dart';
+import 'package:flutter_widget/fab_menu/circular_animated_fab_menu.dart';
 
 void main() {
   runApp(MaterialApp(
@@ -12,6 +13,6 @@ void main() {
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(appBar: null, body: AppbarWithTabLayout());
+    return const Scaffold(appBar: null, body: CircularFabMenu());
   }
 }
