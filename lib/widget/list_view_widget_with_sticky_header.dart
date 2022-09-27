0296ff88cfaf7649c854989c27a32d9678751a82
anikrakib <anikrakib1998@gmail.com>
2022-09-27 13:42:41 +0600

    ListView with Sticky Header Animation

--- lib/widget/list_view_widget_with_sticky_header.dart
+++ lib/widget/list_view_widget_with_sticky_header.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:sticky_headers/sticky_headers.dart';
+import 'package:palette_generator/palette_generator.dart';
 
 class ListViewWidgetWithStickyHeader extends StatelessWidget {
   const ListViewWidgetWithStickyHeader({Key? key}) : super(key: key);
@@ -10,27 +11,38 @@ class ListViewWidgetWithStickyHeader extends StatelessWidget {
           child: ListView.builder(
             shrinkWrap: true,
             itemCount: 100,
-            itemBuilder: (context, index) => StickyHeader(
-              header: Container(
-                width: double.infinity,
-                height: 60,
-                color: Colors.purple,
-                padding: const EdgeInsets.all(8.0),
-                child: Center(
-                  child: Text(
-                    'Sticky Header ${(index + 1)}',
-                    style: const TextStyle(fontSize: 22, color: Colors.white),
+            itemBuilder: (context, index) => StickyHeaderBuilder(
+              overlapHeaders: true,
+              builder: (context, stuckAmount) {
+                stuckAmount = 1 - stuckAmount.clamp(0, 1);
+                return Container(
+                  width: double.infinity,
+                  height: 60,
+                  color: Color.lerp(Colors.grey.shade900.withOpacity(0.6 + stuckAmount * 0.4), Colors.purple.shade700, stuckAmount),
+                  padding: const EdgeInsets.all(8.0),
+                  child: Center(
+                    child: Text(
+                      'Sticky Header #${(index + 1)}',
+                      style: const TextStyle(fontSize: 22, color: Colors.white),
+                    ),
                   ),
-                ),
-              ),
+                );
+              },
               content: Image.network(
                 'https://source.unsplash.com/random?sig=$index',
                 fit: BoxFit.cover,
                 width: double.infinity,
-                height: 200,
+                height: 250,
               ),
             ),
           ),
         ),
       );
+}
+
+// Calculate dominant color from ImageProvider
+Future<Color> getImagePalette (ImageProvider imageProvider) async {
+  final PaletteGenerator paletteGenerator = await PaletteGenerator
+      .fromImageProvider(imageProvider);
+  return paletteGenerator.dominantColor!.color;
 }
