56d784578b18921d6d5d11214e40bb3a7f496817
anikrakib <anikrakib1998@gmail.com>
2022-09-29 01:12:51 +0600

    Custom Scale Picker With Custom Marker

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_widget/widget/staggered_gridview_widget.dart';
+import 'package:flutter_widget/widget/custom_ruler_picker_widget.dart';
 
 void main() {
   runApp(const MaterialApp(
@@ -14,6 +14,6 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(appBar: null, body: StaggeredGridViewWidget());
+    return const Scaffold(appBar: null, body: CustomRulerPickerWidget());
   }
 }
